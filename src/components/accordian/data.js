a054a05939f695a121844d814bc2f6793307e7cf
Aaditya-300 <aadityamusale5@gmail.com>
2025-05-17 20:34:31 +0530

    first app

---
+++ src/components/accordian/data.js
@@ -0,0 +1,23 @@
+const data = [
+  {
+    id: "1",
+    question: "Can I be first question",
+    answer: "no",
+  },
+  {
+    id: "2",
+    question: "Can I be second question",
+    answer: "yes",
+  },
+  {
+    id: "3",
+    question: "Can I be third question",
+    answer: "no",
+  },
+  {
+    id: "4",
+    question: "Can I be fourth question",
+    answer: "no",
+  },
+];
+export default data;
